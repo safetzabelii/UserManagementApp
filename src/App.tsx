7182cf47ce49616a98289e0c067ee457fefb7b5a
safetzabelii <sz51042@ubt-uni.net>
2025-10-01 12:00:55 -0400

    Revert "Updates"
    This reverts commit c4ff617b216e358552db4f8bddb68eb8bf2f934a.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,35 @@
-import { Routes, Route } from 'react-router-dom';
-import UserList from './pages/UserList';
-import UserDetails from './pages/UserDetails';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div className="bg-slate-50 min-h-screen">
-      <header className="bg-white shadow-sm">
-        <nav className="container mx-auto px-4 py-4">
-          <h1 className="text-2xl font-bold text-slate-800">User Management App</h1>
-        </nav>
-      </header>
-      <main className="container mx-auto p-4">
-        <Routes>
-          <Route path="/" element={<UserList/>} />
-          <Route path="/user/:id" element={<UserDetails />} />
-        </Routes>
-      </main>
-    </div>
-  );
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { store, persistor } from './redux/store.ts';
-import { PersistGate } from 'redux-persist/integration/react';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
-  </React.StrictMode>,
-);
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
